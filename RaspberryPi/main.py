f7b221b42169bae931dbeedb776c1b4ffb0b372d
Octavian-Tiberiu NISTORA <octavian.nistora@gmail.com>
2024-05-22 20:38:41 +0300

    Added additional comments to the accelerometer process function

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -1,9 +1,6 @@
 from math import nan
 from multiprocessing import Process, Pipe
-from pyexpat import model
 from time import sleep
-from turtle import speed
-from typing import IO
 
 from sympy import sec
 
@@ -137,29 +134,29 @@ def object_detection_camera(output): # Function to detect objects in the camera 
         for box in results[0].boxes: # Iterate over the detected objects
             class_id = box.cls # Get the class ID of the detected object
             signs_list += [model.names[int(class_id)]] # Get the class name of the detected object and add it to the list of detected objects
-
+            # Sign names are kept as they are in order to preserve as much information as possible
         output.send(signs_list) # Send the list of detected objects to the main process
 
-def accelerometer_sencor_process(output): # Function to measure the acceleration in the x, y, and z directions
+def accelerometer_sensor_process(output): # Function to measure the acceleration in the x, y, and z directions
     import smbus # Import the smbus library used to interact with the I2C bus
     import time # Import the time library used for delays
     
-    DEVICE_ADDRESS = 0x68
-    PWR_MGMT_1   = 0x6B
-    SMPLRT_DIV   = 0x19
-    CONFIG       = 0x1A
-    GYRO_CONFIG  = 0x1B
-    INT_ENABLE   = 0x38
-    ACCEL_XOUT_H = 0x3B
-    ACCEL_YOUT_H = 0x3D
-    ACCEL_ZOUT_H = 0x3F
+    DEVICE_ADDRESS	= 0x68 # I2C address of the MPU6050 sensor
+    SMPLRT_DIV		= 0x19 # Address of Sample Rate Divider register
+    CONFIG			= 0x1A # Address of Configuration register
+    ACCEL_CONFIG	= 0x1C # Address of Accelerometer Configuration register
+    ACCEL_XOUT_H	= 0x3B # Address of X-axis Acceleration Data register
+    ACCEL_YOUT_H	= 0x3D # Address of Y-axis Acceleration Data register
+    ACCEL_ZOUT_H	= 0x3F # Address of Z-axis Acceleration Data register
+    PWR_MGMT_1		= 0x6B # Address of Power Management 1 register
+    PWR_MGMT_2		= 0x6C # Address of Power Management 2 register
     
     def init_mpu(bus, device_address): # Function to initialize the MPU6050 sensor
-        bus.write_byte_data(device_address, SMPLRT_DIV, 7)
-        bus.write_byte_data(device_address, PWR_MGMT_1, 1)
-        bus.write_byte_data(device_address, CONFIG, 0)
-        bus.write_byte_data(device_address, GYRO_CONFIG, 24)
-        bus.write_byte_data(device_address, INT_ENABLE, 1)
+        bus.write_byte_data(device_address, SMPLRT_DIV, 0x07) # Set the sample rate divider to 7
+        bus.write_byte_data(device_address, CONFIG, 0x00) # Set the digital low pass filter to 0 and disable the external frame synchronization
+        bus.write_byte_data(device_address, ACCEL_CONFIG, 0x08) # Set the full scale range of the accelerometer to ±4g
+        bus.write_byte_data(device_address, PWR_MGMT_1, 0x0C) # Disable the temperature sensor and set the clock source to the external 32.768 kHz oscillator
+        bus.write_byte_data(device_address, PWR_MGMT_2, 0x07) # Set the gyroscope sensors (X, Y and Z axes) in standby mode
     
     def read_data_register(bus, device_address, address): # Function to read the data from a register
         high = bus.read_byte_data(device_address, address) # Read the high byte
@@ -174,6 +171,7 @@ def accelerometer_sencor_process(output): # Function to measure the acceleration
     try: # Try to read the acceleration data from the MPU6050 sensor
         bus = smbus.SMBus(1) # Initialize the I2C bus
         init_mpu(bus, DEVICE_ADDRESS) # Initialize the MPU6050 sensor
+        LSBg = 16384.0 / (((bus.read_byte_data(DEVICE_ADDRESS, ACCEL_CONFIG) >> 3) & 0x03) + 1) # Calculate the LSB sensitivity of the accelerometer
 
         while True: # Infinite loop to continuously measure the acceleration
             try:
@@ -185,9 +183,9 @@ def accelerometer_sencor_process(output): # Function to measure the acceleration
             except: # If there is any other error, raise the error to stop the process
                 raise
 
-            Ax = acc_x/16384.0 # Convert the raw acceleration data to g-force
-            Ay = acc_y/16384.0 # Convert the raw acceleration data to g-force
-            Az = acc_z/16384.0 # Convert the raw acceleration data to g-force
+            Ax = acc_x/LSBg # Convert the raw acceleration data to g-force
+            Ay = acc_y/LSBg # Convert the raw acceleration data to g-force
+            Az = acc_z/LSBg # Convert the raw acceleration data to g-force
             output.send([Ax, Ay, Az]) # Send the acceleration data to the main process
             time.sleep(0.01) # Wait for 0.01 seconds before starting the next iteration to prevent the process from consuming too much CPU power and becoming unstable
     except: # If there is an error, close the I2C bus and the output pipe
@@ -211,11 +209,14 @@ if __name__ == "__main__":
     camera_process.start() # Start the object detection camera process
 
     parent_accelerometer_input, child_accelerometer_output = Pipe(False) # Create a pipe from the accelerometer process to the main process
-    accelerometer_process = Process(target=accelerometer_sencor_process, args=(child_accelerometer_output,)) # Create a process for the accelerometer
+    accelerometer_process = Process(target=accelerometer_sensor_process, args=(child_accelerometer_output,)) # Create a process for the accelerometer
     accelerometer_process.start() # Start the accelerometer process
 
     try:
         crashes = 0 # Variable to store the number of crashes
+        stop_signs = ["regulatory - stop"] # List of signs that require the vehicle to stop
+        reduce_signs = ["regulatory - yield", "warning - roadworks", "warning - children", "warning - pedestrians crossing", "warning - railroad crossing", "warning - railroad crossing with barriers", "warning - railroad crossing without barriers"] # List of signs that require the vehicle to reduce its speed
+
         while True: # Infinite loop to continuously control the speed of the DC motor based on the proximity sensor and the detected objects
             distance = nan # Variable to store the distance between the sensor and an object, initialized to NaN in case the proximity sensor process has not sent any data
             signs = [] # List to store the detected objects, initialized to an empty list in case the object detection camera process has not sent any data
@@ -226,28 +227,10 @@ if __name__ == "__main__":
             while (parent_proximity_input.poll()): # Parse the input buffer until it is empty
                 distance = parent_proximity_input.recv() # Receive the next distance value from the proximity sensor process
             print(f"Distance: {distance}") # Print the distance between the sensor and an object for debugging purposes
-
-            if distance < 0.5: # If the distance is less than 0.5 meters, go backwards at half speed
-                speed = -0.5
-            elif distance > 1.0: # If the distance is greater than 1.0 meters, go forwards
-                if distance < 3.0: # If the distance is less than 3.0 meters, go at half speed
-                    speed = 0.5
-                else: # If the distance is greater than 3.0 meters, go at full speed
-                    speed = 1.0
             
             while (parent_camera_input.poll()): # Parse the input buffer until it is empty
                 signs = parent_camera_input.recv() # Receive the next list of detected objects from the object detection camera process
             print(f"Signs: {signs}") # Print the detected objects for debugging purposes
-            
-            if any(danger_sign in signs for danger_sign in ["regulatory - stop"]): # If a stop sign is detected, stop the vehicle
-                speed = 0.0
-            elif any(danger_sign in signs for danger_sign in ["regulatory - yield", "warning - roadworks",
-                                                              "warning - children", "warning - pedestrians crossing",
-                                                              "warning - railroad crossing",
-                                                              "warning - railroad crossing with barriers",
-                                                              "warning - railroad crossing without barriers"]): # If a yield or a warning sign is detected, slow down the vehicle
-                if speed > 0.0: # Slow down the vehicle if it is moving forwards
-                    speed = 0.5
 
             while (parent_accelerometer_input.poll()): # Parse the input buffer until it is empty
                 acceleration = parent_accelerometer_input.recv() # Receive the next acceleration value from the accelerometer process
@@ -257,12 +240,35 @@ if __name__ == "__main__":
                 total_acceleration += a**2 # Add the square of each acceleration component to the total acceleration
             total_acceleration = total_acceleration**0.5 # Take the square root of the total acceleration
             print(f"Total Acceleration: {total_acceleration}") # Print the total acceleration for debugging purposes
-            
+
+            if total_acceleration > 1.2: # If the total acceleration is greater than 1.2 g, increment the number of crashes
+                crashes += 1
+            print(f"Crashes: {crashes}")
+
+            if crashes == 0: # If no crash has occurred, control the speed of the DC motor based on the distance and the detected objects
+                if distance < 0.5: # If the distance is less than 0.5 meters, go backwards at half speed
+                    speed = -0.5
+                elif distance > 1.0: # If the distance is greater than 1.0 meters, go forwards
+                    if distance < 3.0: # If the distance is less than 3.0 meters, go at half speed
+                        speed = 0.5
+                    else: # If the distance is greater than 3.0 meters, go at full speed
+                        speed = 1.0
+                # No need for else statement as speed is already set to 0.0
+
+                if any(' - '.join(sign.split('--')[:-1]) in stop_signs for sign in signs): # If a stop sign is detected, stop the vehicle
+                    speed = 0.0
+                elif any(' - '.join(sign.split('--')[:-1]) in reduce_signs for sign in signs): # If a yield or a warning sign is detected, slow down the vehicle
+                    if speed > 0.0: # Slow down the vehicle if it is moving forwards
+                        speed = 0.5
+            else:
+                speed = 0.0 # Stop the vehicle if a crash has occurred
+
             parent_motor_output.send(speed) # Send the speed of the DC motor to the DC motor process
 
             print() # Print an empty line to separate the iterations for debugging purposes
             sleep(0.4) # Wait for 0.4 seconds before starting the next iteration to prevent the process from consuming too much CPU power and becoming unstable
     except:
+        parent_motor_output.close()
         parent_proximity_input.close() # Close the input pipe from the proximity sensor process
         parent_proximity_output.close() # Close the output pipe to the proximity sensor process
         proximity_sensor_process.join() # Wait for the proximity sensor process to end
